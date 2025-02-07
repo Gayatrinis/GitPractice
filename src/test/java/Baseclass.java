8916b8982176095b2200c3b4358635770fee43b2
Gayatri <gayatri.lalitha.nis@gmail.com>
2025-02-07 03:07:49 -0500

    Architect from US development branch

--- src/test/java/Baseclass.java
+++ src/test/java/Baseclass.java
@@ -1,13 +1,11 @@
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -47,42 +45,48 @@ public class Baseclass {
         ( (JavascriptExecutor) driver).executeScript("mobile: longClickGesture", ImmutableMap.of(
                "elementId", ((RemoteWebElement) ele).getId(),"duration",4000));
     }
-   public void swipeAction(WebElement ele,String direction)
-    {
-      ((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
-              "elementId", ((RemoteWebElement)ele).getId(),
-             "direction", direction,
+   public void swipeAction(WebElement ele,String direction) {
+       ((JavascriptExecutor) driver).executeScript("mobile: swipeGesture", ImmutableMap.of(
+               "elementId", ((RemoteWebElement) ele).getId(),
+               "direction", direction,
                "percent", 0.4
 
-        ));
-        @Test
-        void DragNDropTest() throws MalformedURLException, InterruptedException
-        {
+       ));
+       public void DragNDropTest () throws MalformedURLException, InterruptedException
+       {
 
-            driver.findElement(new AppiumBy.ByAccessibilityId("Views")).click();
-            driver.findElement(new AppiumBy.ByAccessibilityId("Drag and Drop")).click();
-            WebElement source=driver.findElement(By.id("io.appium.android.apis:id/drag_dot_1"));
-            ((JavascriptExecutor) driver).executeScript("mobile: dragGesture", ImmutableMap.of(
-                    "elementId", ((RemoteWebElement) source).getId(),
-                    "endX", 625,
-                    "endY", 640
-            ));
-    }
+           driver.findElement(new AppiumBy.ByAccessibilityId("Views")).click();
+           driver.findElement(new AppiumBy.ByAccessibilityId("Drag and Drop")).click();
+           WebElement source = driver.findElement(By.id("io.appium.android.apis:id/drag_dot_1"));
+           ((JavascriptExecutor) driver).executeScript("mobile: dragGesture", ImmutableMap.of(
+                   "elementId", ((RemoteWebElement) source).getId(),
+                   "endX", 625,
+                   "endY", 640
+           ));
+           @Test
+           void Miscellaneous() throws MalformedURLException
+           {
+               Activity activity= new Activity("io.appium.android.apis","io.appium.android.apis.preference.PreferenceDependencies");
+               ((JavascriptExecutor) driver).executeScript("mobile: startActivity", ImmutableMap.of("intent"," io.appium.android.apis/io.appium.android.apis.preference.PreferenceDependencies"
+
+               ));
+               DeviceRotation potrait= new DeviceRotation(90,90,0);
+               driver.rotate(potrait);
+
+           }
 
 
+       public static Double getFormattedAmount (String amount) throws NumberFormatException
+       {
+           Double price1 = parseDouble(amount.substring(1));
+           price1 = price1;
+           return price1;
+       }
+       @AfterClass
+       void teardown ()
+       {
+           driver.quit();
+           service.stop();
 
-
-    public static Double getFormattedAmount(String amount) throws NumberFormatException
-    {
-     Double price1= parseDouble(amount.substring(1));
-        price1 = price1;
-        return price1;
+       }
    }
-    @AfterClass
-    void teardown()
-    {
-        driver.quit();
-        service.stop();
-
-    }
-}
